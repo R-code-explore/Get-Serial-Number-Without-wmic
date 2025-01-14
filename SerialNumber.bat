52ed67ed80f89b37f05efb20e5bdb9aa9466ff6a
R-code-explore <89782067+R-code-explore@users.noreply.github.com>
2025-01-14 16:30:49 +0100

    Comments update

--- SerialNumber.bat
+++ SerialNumber.bat
@@ -1,10 +1,10 @@
 @echo off
-:: Définir la variable pour le chemin du bureau
+:: Variable define for desktop's location || Définition de la variable pour le chemin du bureau
 set "Desktop=%USERPROFILE%\Desktop"
 
-:: Exécuter PowerShell pour récupérer le numéro de série
+:: PowerShell execution to get PC's serial number || Exécuter PowerShell pour récupérer le numéro de série
 powershell -Command "Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber | Out-File -FilePath '%Desktop%\NumeroDeSerie.txt' -Encoding utf8; Get-Content '%Desktop%\NumeroDeSerie.txt' | Set-Clipboard"
 
-:: Message de confirmation
-echo Le numéro de série a été copié dans le presse-papier et enregistré sur le bureau dans 'NumeroDeSerie.txt'.
+:: Confirmation message || Message de confirmation
+echo The serial number has been copied to the clipboard and saved in the file “NumeroDeSerie.txt” on the desktop -- Le numéro de série a été copié dans le presse-papier et enregistré sur le bureau dans 'NumeroDeSerie.txt'.
 pause
